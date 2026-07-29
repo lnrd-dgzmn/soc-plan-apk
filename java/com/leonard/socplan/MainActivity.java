e1856267cc332dc4e372af7db3ce6d35da72d455
Leonard De Guzman <ldgzmn0419@gmail.com>
2026-07-28 20:29:33 -0500

    Add files via upload

---
+++ java/com/leonard/socplan/MainActivity.java
@@ -0,0 +1,40 @@
+package com.leonard.socplan;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.activity.OnBackPressedCallback;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView web;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        web = new WebView(this);
+        WebSettings settings = web.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);   // lets your ticked progress save
+        web.setWebViewClient(new WebViewClient());
+        web.loadUrl("file:///android_asset/index.html");
+        setContentView(web);
+
+        // Back button navigates WebView history before leaving the app.
+        getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                if (web.canGoBack()) {
+                    web.goBack();
+                } else {
+                    setEnabled(false);
+                    getOnBackPressedDispatcher().onBackPressed();
+                }
+            }
+        });
+    }
+}
